b45971db22d789228a6715054a3a12b67c7edc60
Samuel Tremblay <samuel.tremblay65@gmail.com>
2023-09-21 17:57:10 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from robotics import *
+
+a = q2d(0, 10, 10)
+
+print(a)
---
+++ robotics.py
@@ -0,0 +1,20 @@
+from math import sin;
+from math import cos;
+
+
+def q2d(x_t, y_t, theta):
+    a = [[cos(theta), sin(theta), x_t], [sin(theta), cos(theta), y_t], [0, 0, 1]]
+    return a
+
+def q3d():
+    print("Not implemented")
+
+def matrix_multiply2d(a, b):
+    print("Not implemented")
+    return ""
+
+def matrix_multiply3d(a,b):
+    print("Not implemented")
+
+def inv2d(a):
+    print("Not implemented")
